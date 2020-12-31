53b5719303ff92c075f907c3e63caeb85755f73a
Luke Lombardi <luke@smartshare.io>
2020-12-31 17:49:19 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup
 
 setup(
-    name='PyDrive',
-    version='1.3.1',
-    author='JunYoung Gwak',
-    author_email='jgwak@dreamylab.com',
-    maintainer='Robin Nabel',
-    maintainer_email='rnabel@ucdavis.edu',
-    packages=['pydrive', 'pydrive.test'],
-    url='https://github.com/gsuitedevs/PyDrive',
-    license='LICENSE',
-    description='Google Drive API made easy.',
-    long_description=open('README.rst').read(),
+    name="PyDrive",
+    version="1.3.1",
+    author="JunYoung Gwak",
+    author_email="jgwak@dreamylab.com",
+    maintainer="Robin Nabel",
+    maintainer_email="rnabel@ucdavis.edu",
+    packages=["pydrive", "pydrive.test"],
+    url="https://github.com/luke-lombardi/PyDrive.git",
+    license="LICENSE",
+    description="Google Drive API made easy.",
+    long_description=open("README.rst").read(),
     install_requires=[
         "google-api-python-client >= 1.2",
         "oauth2client >= 4.0.0",
